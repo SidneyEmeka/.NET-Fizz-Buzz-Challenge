add7d3f971224f12f1e7fdc4c138a6634ba5ac82
Sidswipe <sidswipexyz@gmail.com>
2024-02-22 15:49:02 +0100

    Fizz BUZZ in C#

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Create the for loop that counts from 1 to 100
+for (int i = 1; i <= 100; i++)
+{
+// Using If-else if-else statements create the conditions
+    if (i % 3 == 0 && i % 5 == 0) Console.WriteLine($"{i} - Fizz Buzz");
+    else if (i % 5 == 0) Console.WriteLine($"{i} - Buzz");
+    else if(i % 3 == 0) Console.WriteLine($"{i} - Fizz");
+   else Console.WriteLine(i);
+}
